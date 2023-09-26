91c8fc776803456ed26dc8b2da0fa05d595283b8
nico <nc@nicores.de>
2023-09-26 14:46:09 +0100

    added files to allow for installation via pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="gethurricaneloss",
+    version="0.1",
+    packages=find_packages(),
+    url="https://github.com/ncerutti/gethurricaneloss",
+    install_requires=[
+        "numba==0.58.0",
+        "numpy==1.25.2",
+        "pytest==7.4.2",
+        "tqdm==4.66.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "gethurricaneloss = src.gethurricaneloss:main",
+        ],
+    },
+)
